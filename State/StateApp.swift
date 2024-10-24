9e74e1f2f6b34c17a56cf163b4d77d97e7e1c89d
Jerry Feng <jerry.feng@viacomcontractor.com>
2024-10-24 15:07:08 -0300

    Initial Commit

---
+++ State/StateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateApp.swift
+//  State
+//
+//  Created by Jerry Feng on 2024-10-24.
+//
+
+import SwiftUI
+
+@main
+struct StateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
